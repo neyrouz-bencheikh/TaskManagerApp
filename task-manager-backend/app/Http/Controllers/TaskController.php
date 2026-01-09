310243c335b14d0912ffda80085fa8f49b5e11c5
neyrouz-bencheikh <neyrouzbencheikh4@gmail.com>
2026-01-09 15:22:42 +0100

    Create TaskController
    gérer les tâches : création, lecture, mise à jour et suppression (CRUD).

---
+++ task-manager-backend/app/Http/Controllers/TaskController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Task::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        return Task::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return Task::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $task = Task::findOrFail($id); 
+        $task->update($request->all()); 
+        return $task;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Task::destroy($id); 
+        return response()->json(['message' => 'Task deleted']);
+    }
+}
