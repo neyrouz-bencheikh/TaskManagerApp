b5b3f7b974d7feeffb79d9d3db0a9aab7c579dc6
neyrouz-bencheikh <neyrouzbencheikh4@gmail.com>
2026-01-09 17:52:35 +0100

    Correcting mistakes
    ajouter use App\Models\Task; et réecrire la fonction store et la class task dans le model

--- task-manager-backend/app/Http/Controllers/TaskController.php
+++ task-manager-backend/app/Http/Controllers/TaskController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Task;
+
+
 class TaskController extends Controller
 {
     /**
@@ -17,8 +20,8 @@ class TaskController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
+    public function store(Request $request) 
+    { 
         return Task::create($request->all());
     }
 
